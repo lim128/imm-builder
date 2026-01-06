92085c831d6bb16f0681e50b2674303f6e85b3a9
lim128 <32699966+lim128@users.noreply.github.com>
2026-01-06 14:29:40 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,3 +4,4 @@ git clone --depth 1 -b v5 --single-branch https://github.com/sbwml/openwrt_hello
 #git clone --depth 1 -b v5 --single-branch https://github.com/sbwml/luci-app-mosdns.git package/luci-app-mosdns
 git clone --depth 1 -b master https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone --depth 1 -b main --single-branch https://github.com/sbwml/luci-app-openlist2.git package/luci-app-openlist2
+git clone --depth 1 -b master https://github.com/tty228/luci-app-wechatpush.git package/luci-app-wechatpush
