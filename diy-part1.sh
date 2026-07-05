5c916db93c950d044eabdc95a7c7fe836641d7db
lim128 <32699966+lim128@users.noreply.github.com>
2026-07-05 08:58:42 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #git clone --depth 1 -b beta --single-branch https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone --depth 1 https://github.com/kenzok8/openwrt-daede.git package/daede
-git clone --depth 1 -b v5 --single-branch https://github.com/sbwml/openwrt_helloworld.git package/helloworld && rm -rf package/helloworld/{luci-app-homeproxy,lua-neturl}
+git clone --depth 1 -b v5 --single-branch https://github.com/sbwml/openwrt_helloworld.git package/helloworld && rm -rf package/helloworld/{luci-app-homeproxy,lua-neturl,dae,daed}
 git clone --depth 1 -b v5 --single-branch https://github.com/sbwml/luci-app-mosdns.git package/luci-app-mosdns
 git clone --depth 1 -b master https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone --depth 1 -b main --single-branch https://github.com/sbwml/luci-app-openlist2.git package/luci-app-openlist2
